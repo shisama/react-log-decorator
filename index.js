a99909ea95722145f54386330efbac3ece0d194c
Masashi Hirano <cherrydog07@gmail.com>
2018-02-26 23:35:34 +0900

    Add: source code and package.json

---
+++ index.js
@@ -0,0 +1,32 @@
+export default function(logging = true) {
+  return function log(target, name, descriptor) {
+    const func = descriptor.value;
+    descriptor.value = function (...args) {
+      if (logging === false) {
+        return func.bind(this)(this.props);
+      }
+      const log = console.log;
+      log("class: %c" + this.constructor.name, 'color: magenta;');
+      log("  method: %c" + name, 'color: green;');
+      log("    props:" + JSON.stringify(this.props));
+      log("    state:" + JSON.stringify(this.state));
+      const [propsArg, stateArg] = args;
+      switch (name) {
+        case "componentWillReceiveProps":
+          log("    nextProps:" + JSON.stringify(propsArg));
+          break;
+        case "shouldComponentUpdate":
+        case "componentWillUpdate":
+          log("    nextProps:" + JSON.stringify(propsArg));
+          log("    nextState:" + JSON.stringify(stateArg));
+          break;
+        case "componentDidUpdate":
+          log("    prevProps:" + JSON.stringify(propsArg));
+          log("    prevState:" + JSON.stringify(stateArg));
+          break;
+      }
+      return func.bind(this)(this.props);
+    };
+    return descriptor;
+  }
+}
